e5d4240605390ab3d71af6e4d0b5047fcd2f6999
Aman gwari <amangwari29sep@gmail.com>
2024-10-19 01:43:55 +0530

    commit new ui changes

--- indexjs/main.js
+++ indexjs/main.js
@@ -1,6 +1,9 @@
-(function($){
-	"use strict";
-    
+(function ($) {
+    "use strict";
+
+
+    AOS.init();
+
     // Mean Menu
     jQuery('.mean-menu').meanmenu({
         meanScreenWidth: "991"
@@ -10,11 +13,11 @@
     $('select').niceSelect();
 
     // Header Sticky
-    $(window).on('scroll',function() {
-        if ($(this).scrollTop() > 120){  
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 120) {
             $('.navbar-area').addClass("is-sticky");
         }
-        else{
+        else {
             $('.navbar-area').removeClass("is-sticky");
         }
     });
@@ -30,9 +33,9 @@
     });
 
     // Counter JS
-    $('.odometer').appear(function(e) {
+    $('.odometer').appear(function (e) {
         var odo = $(".odometer");
-        odo.each(function() {
+        odo.each(function () {
             var countNumber = $(this).attr("data-count");
             $(this).html(countNumber);
         });
@@ -68,8 +71,8 @@
     });
 
     // FAQ Accordion
-    $(function() {
-        $('.accordion').find('.accordion-title').on('click', function(){
+    $(function () {
+        $('.accordion').find('.accordion-title').on('click', function () {
             // Adds Active Class
             $(this).toggleClass('active');
             // Expand or Collapse This Panel
@@ -77,20 +80,20 @@
             // Hide The Other Panels
             $('.accordion-content').not($(this).next()).slideUp('fast');
             // Removes Active Class From Other Titles
-            $('.accordion-title').not($(this)).removeClass('active');		
+            $('.accordion-title').not($(this)).removeClass('active');
         });
     });
-    
+
     // Input Plus & Minus Number JS
-    $('.input-counter').each(function() {
+    $('.input-counter').each(function () {
         var spinner = jQuery(this),
-        input = spinner.find('input[type="text"]'),
-        btnUp = spinner.find('.plus-btn'),
-        btnDown = spinner.find('.minus-btn'),
-        min = input.attr('min'),
-        max = input.attr('max');
-        
-        btnUp.on('click', function() {
+            input = spinner.find('input[type="text"]'),
+            btnUp = spinner.find('.plus-btn'),
+            btnDown = spinner.find('.minus-btn'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.on('click', function () {
             var oldValue = parseFloat(input.val());
             if (oldValue >= max) {
                 var newVal = oldValue;
@@ -100,7 +103,7 @@
             spinner.find("input").val(newVal);
             spinner.find("input").trigger("change");
         });
-        btnDown.on('click', function() {
+        btnDown.on('click', function () {
             var oldValue = parseFloat(input.val());
             if (oldValue <= min) {
                 var newVal = oldValue;
@@ -116,8 +119,8 @@
     (function ($) {
         $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
         $('.tab ul.tabs li a').on('click', function (g) {
-            var tab = $(this).closest('.tab'), 
-            index = $(this).closest('li').index();
+            var tab = $(this).closest('.tab'),
+                index = $(this).closest('li').index();
             tab.find('ul.tabs > li').removeClass('current');
             $(this).closest('li').addClass('current');
             tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
@@ -128,11 +131,11 @@
 
     // Progress Bar JS
     $(".progress-bar").loading();
-    
+
     // Subscribe form
     $(".newsletter-form").validator().on("submit", function (event) {
         if (event.isDefaultPrevented()) {
-        // handle the invalid form...
+            // handle the invalid form...
             formErrorSub();
             submitMSGSub(false, "Please enter your email correctly.");
         } else {
@@ -140,7 +143,7 @@
             event.preventDefault();
         }
     });
-    function callbackFunction (resp) {
+    function callbackFunction(resp) {
         if (resp.result === "success") {
             formSuccessSub();
         }
@@ -148,21 +151,21 @@
             formErrorSub();
         }
     }
-    function formSuccessSub(){
+    function formSuccessSub() {
         $(".newsletter-form")[0].reset();
         submitMSGSub(true, "Thank you for subscribing!");
-        setTimeout(function() {
+        setTimeout(function () {
             $("#validator-newsletter").addClass('hide');
         }, 4000)
     }
-    function formErrorSub(){
+    function formErrorSub() {
         $(".newsletter-form").addClass("animated shake");
-        setTimeout(function() {
+        setTimeout(function () {
             $(".newsletter-form").removeClass("animated shake");
         }, 1000)
     }
-    function submitMSGSub(valid, msg){
-        if(valid){
+    function submitMSGSub(valid, msg) {
+        if (valid) {
             var msgClasses = "validation-success";
         } else {
             var msgClasses = "validation-danger";
@@ -177,14 +180,14 @@
 
     // Count Time 
     function makeTimer() {
-        var endTime = new Date("September 30, 2025 17:00:00 PDT");			
+        var endTime = new Date("September 30, 2025 17:00:00 PDT");
         var endTime = (Date.parse(endTime)) / 1000;
         var now = new Date();
         var now = (Date.parse(now) / 1000);
         var timeLeft = endTime - now;
-        var days = Math.floor(timeLeft / 86400); 
+        var days = Math.floor(timeLeft / 86400);
         var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
         var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
         if (hours < "10") { hours = "0" + hours; }
         if (minutes < "10") { minutes = "0" + minutes; }
@@ -194,39 +197,39 @@
         $("#minutes").html(minutes + "<span>Minutes</span>");
         $("#seconds").html(seconds + "<span>Seconds</span>");
     }
-    setInterval(function() { makeTimer(); }, 1000);
-    
+    setInterval(function () { makeTimer(); }, 1000);
+
     // Go to Top
-    $(function(){
+    $(function () {
         // Scroll Event
-        $(window).on('scroll', function(){
+        $(window).on('scroll', function () {
             var scrolled = $(window).scrollTop();
             if (scrolled > 600) $('.go-top').addClass('active');
             if (scrolled < 600) $('.go-top').removeClass('active');
-        });  
+        });
         // Click Event
-        $('.go-top').on('click', function() {
-            $("html, body").animate({ scrollTop: "0" },  0);
+        $('.go-top').on('click', function () {
+            $("html, body").animate({ scrollTop: "0" }, 0);
         });
     });
 
     // WOW JS
-	$(window).on ('load', function (){
-        if ($(".wow").length) { 
+    $(window).on('load', function () {
+        if ($(".wow").length) {
             var wow = new WOW({
-            boxClass:     'wow',      // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset:       20,          // distance to the element when triggering the animation (default is 0)
-            mobile:       true,       // trigger animations on mobile devices (default is true)
-            live:         true,       // act on asynchronously loaded content (default is true)
-          });
-          wow.init();
+                boxClass: 'wow',      // animated element css class (default is wow)
+                animateClass: 'animated', // animation css class (default is animated)
+                offset: 20,          // distance to the element when triggering the animation (default is 0)
+                mobile: true,       // trigger animations on mobile devices (default is true)
+                live: true,       // act on asynchronously loaded content (default is true)
+            });
+            wow.init();
         }
     });
-    
+
     // Preloader Area
-	$(window).on('load', function() {
-		$('.preloader').fadeOut();
-	});
-    
+    // $(window).on('load', function() {
+    // 	$('.preloader').fadeOut();
+    // });
+
 }(jQuery));
